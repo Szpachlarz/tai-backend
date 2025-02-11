05d8e9f7c1ffc38fb6082f0f8ec98e492530bffd
Szpachlarz <kl.kolakowska@gmail.com>
2025-02-12 00:54:30 +0100

    item upload fix

--- tai-shop/Dtos/Item/UpdateItemDto.cs
+++ tai-shop/Dtos/Item/UpdateItemDto.cs
@@ -4,13 +4,10 @@ namespace tai_shop.Dtos.Item
 {
     public class UpdateItemDto
     {
-        [Required]
         public string Name { get; set; }
-        [Required]
         public string Description { get; set; }
-        [Required]
         public decimal Price { get; set; }
-        public IEnumerable<IFormFile> Photos { get; set; }
-        public List<int> PhotosToDelete { get; set; }
+        public IEnumerable<IFormFile>? Photos { get; set; }
+        public List<int>? PhotosToDelete { get; set; }
     }
 }
